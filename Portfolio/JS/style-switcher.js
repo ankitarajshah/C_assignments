39e0c0ca339f6e1e8039ef640d202eabbae606be
ankitarajshah <k_anku@yahoo.co.in>
2022-12-18 01:17:42 -0800

    Portfolio

---
+++ Portfolio/JS/style-switcher.js
@@ -0,0 +1,7 @@
+/*---------------Toogle style switcher---------------*/
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click",()=>{
+	document.querySelector(".style-switcher").classList.toggle("open");
+
+
+})
